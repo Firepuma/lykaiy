b478497553db832dbef31aa0961963ebe2a2f98e
dirkliu <liuqi0702@126.com>
2014-11-05 22:01:42 +0800

    添加栏目导入

---
+++ cms/data/models/ky_content_product.tableinfo.cache.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('IN_XIAOCMS')) exit();
+return array (
+  'primaryKey' => 'id',
+  'fields' => 
+  array (
+    0 => 'id',
+    1 => 'catid',
+    2 => 'content',
+  ),
+);
---
+++ cms/data/models/ky_model.tableinfo.cache.php
@@ -0,0 +1,16 @@
+<?php
+if (!defined('IN_XIAOCMS')) exit();
+return array (
+  'primaryKey' => 'modelid',
+  'fields' => 
+  array (
+    0 => 'modelid',
+    1 => 'typeid',
+    2 => 'modelname',
+    3 => 'tablename',
+    4 => 'listtpl',
+    5 => 'showtpl',
+    6 => 'joinid',
+    7 => 'setting',
+  ),
+);
